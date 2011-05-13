09871318c1cafa66806a8ed17548a98b3fe49eb1
Sergio Cuellar <herrsergio@gmail.com>
2011-05-13 16:44:52 -0500

    Added methods and info about Employee

--- src/PRBAS/Employee.java
+++ src/PRBAS/Employee.java
@@ -3,10 +3,16 @@ package PRBAS;
 public class Employee {
 	private String name;
 	private String id;
+	private String RFC;
+	private String Puesto;
+	private String Dept;
 	
-	public Employee(String name , String id ) {
-		this.name = name;
-		this.id   = id;
+	public Employee(String name , String id, String RFC, String Puesto, String Dept ) {
+		this.name   = name;
+		this.id     = id;
+		this.RFC    = RFC;
+		this.Puesto = Puesto;
+		this.Dept   = Dept;
 	}
 	
 	public String getID() {
@@ -15,6 +21,18 @@ public class Employee {
 	
 	public String getName() {
 		return name;
+	}
+	
+	public String getRFC() {
+		return RFC;
+	}
+	
+	public String getPuesto() {
+		return Puesto;
+	}
+	
+	public String getDept() {
+		return Dept;
 	}
 	
 	public String toString() {
